d49802dd3a85c6e7d1cd1555849f9d457573fa7b
Gourav199 <tewarygourav1997@gmail.com>
2023-06-07 14:05:33 +0530

    minor changes

--- src/login.js
+++ src/login.js
@@ -1,7 +1,6 @@
-import React from 'react'
-import "./css/style.css"
-export default function 
-() {
+import React from 'react';
+import "./css/style.css";
+export default function () {
   return (
     <div>
         <div class="container">
@@ -21,14 +20,7 @@ export default function
 					<i class="button__icon fas fa-chevron-right"></i>
 				</button>				
 			</form>
-			<div class="social-login">
-				<h3>log in via</h3>
-				<div class="social-icons">
-					<a href="#" class="social-login__icon fab fa-instagram"></a>
-					<a href="#" class="social-login__icon fab fa-facebook"></a>
-					<a href="#" class="social-login__icon fab fa-twitter"></a>
-				</div>
-			</div>
+			
 		</div>
 		<div class="screen__background">
 			<span class="screen__background__shape screen__background__shape4"></span>

7d731c0c8aeddd80226091f4538f08ce665ad01a
Gourav199 <tewarygourav1997@gmail.com>
2023-06-07 19:30:44 +0530

    adding route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
 import Login from './login';
+import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <Login />
+    <div>
+       <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
     </div>
+    
   );
 }
 
--- src/login.js
+++ src/login.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./css/style.css";
+import { useHistory } from 'react-router-dom';
+
 export default function Login () {
+   const [email, setemail] = useState("");
+   const [password, setpassword] = useState("");
+   const history = useHistory();
+
+   function userName(event) {
+      setemail(event.target.value)
+      //alert(email)
+   }
+   function userPassword(event) {
+      setpassword(event.target.value)
+      //alert(password)
+   }
+   function submit (){
+     if(email === 'admin' && password === 'admin'){
+      history.push('/dashboard');
+     }
+   }
   return (
     <div>
         <div class="container">
@@ -9,13 +28,13 @@ export default function Login () {
 			<form class="login">
 				<div class="login__field">
 					<i class="login__icon fas fa-user"></i>
-					<input type="text" class="login__input" placeholder="User name / Email" />
+					<input type="text" class="login__input" placeholder="User name / Email" onChange={userName} />
 				</div>
 				<div class="login__field">
 					<i class="login__icon fas fa-lock"></i>
-					<input type="password" class="login__input" placeholder="Password" />
+					<input type="password" class="login__input" placeholder="Password"  onChange={userPassword}/>
 				</div>
-				<button class="button login__submit">
+				<button class="button login__submit" onClick={submit}>
 					<span class="button__text">Log In Now</span>
 					<i class="button__icon fas fa-chevron-right"></i>
 				</button>				
